423c1e42938e0f8d5b4280f01f88f53ed8aa13dd
Darley Peralta <ordeal00s@gmail.com>
2023-06-13 18:09:16 -0400

    text box

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -7,38 +7,67 @@ export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todos:[]
+      todos: [],
+      error: "",
+      tdNameInput: '',
     }
   }
 
-fetchAllTodos() {
-  axios.get(URL)
-    .then(res=>{
-      this.setState({ ...this.state, todos: res.data.data})})
-    .catch(err=>{
-      debugger})
-}
+  onTdNameInputChange = (evt) => {
+    const { value } = evt.target;
+    this.setState({ ...this.state, tdNameInput: value })
+  }
 
-componentDidMount() {
-  this.fetchAllTodos()
-}
+  fetchAllTodos() {
+    axios.get(URL)
+      .then(res => {
+        this.setState({ ...this.state, todos: res.data.data })
+      })
+      .catch(err => {
+        this.setState({ ...this.state, error: err.response.data.message })
+      })
+  }
+
+
+
+
+  postNewTodo = () => {
+    axios.post(URL,{ name: this.state.tdNameInput})
+    .then( res => {
+      this.fetchAllTodos()
+      this.setState({...this.state, tdNameInput: ''})
+    })
+    .catch( err=>{
+      this.setState({ ...this.state, error: err.response.data.message })
+    })
+
+  }
+
+  onTodoFormSubmit = evt => {
+    evt.preventDefault();
+    this.postNewTodo()
+  }
+
+  componentDidMount() {
+    this.fetchAllTodos()
+  }
 
   render() {
     return (
       <div>
-        <div id="error">Error: No error here</div>
+        <div id="error">Error {this.state.error}</div>
         <div id="todos">
           <h2>Todos</h2>
           {
-            this.state.todos.map( todo => {
+            this.state.todos.map(todo => {
               return (
                 <div key={todo.id}>{todo.name}</div>
               )
             })
           }
         </div>
-        <form id="todoForm">
-          <input type="text" placeholder='Type todo'></input>
+        <form id="todoForm" onSubmit={this.onTodoFormSubmit}>
+          <input value={this.state.tdNameInput} onChange={this.onTdNameInputChange} type="text" placeholder='Type todo'></input>
           <input type='submit'></input>
           <button>Clear Completed</button>
 
